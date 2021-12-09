06dec2f4b9454d92a05bab352c1fa4a4620f6730
Keith_hoka <keith_d_hau@hotmail.com>
2021-12-10 10:15:39 +1100

    fixed bug

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cors({
   methods: "GET,POST,PUT,DELETE",
 }));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://travel-blog-ga.netlify.app/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   next();
