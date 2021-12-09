396931673e06993b4656df6669d44b3ac2c66d48
Keith_hoka <keith_d_hau@hotmail.com>
2021-12-09 21:20:40 +1100

    server heroku

--- server.js
+++ server.js
@@ -30,6 +30,11 @@ app.use(cors({
   credentials: true,
   methods: "GET,POST,PUT,DELETE",
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://admiring-shaw-870b70.netlify.app/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(session({
